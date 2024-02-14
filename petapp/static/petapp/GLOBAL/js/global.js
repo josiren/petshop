5f612501396e83fe2d14f16519a9f4ebb01d53ec
Cuda-sudo <egor.novikov666@bk.ru>
2024-02-14 21:24:19 +0300

    create templates for more pages

--- petapp/static/petapp/GLOBAL/js/global.js
+++ petapp/static/petapp/GLOBAL/js/global.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
   const currenturl = window.location.pathname;
-  let pages = ["home-page", "catalog-page", "contact-page", "about-page", "auth-page", "basket-page"];
+  let pages = ["home-page", "catalog-page", "contact-page", "about-page", "auth-page", "basket-page", "reg-page"];
 
   for (let page of pages) {
     let element = document.querySelector("." + page);
     if (page === "home-page" && currenturl === "/") {
       element.classList.add("active-under");
     } else if (currenturl === `/${page.split("-")[0]}/`) {
-      if (page === "basket-page" || page === "auth-page") {
+      if (page === "basket-page" || page === "auth-page" || page === "reg-page") {
         element.classList.add("active-color-swap");
       } else {
         element.classList.add("active-under");
