67ce2af066a6675bf4ba1b1fe4d9e26b1d9e8ed5
Madara <122021584+Dina-Tolkina@users.noreply.github.com>
2024-02-21 12:58:40 +0300

    Added media file settings
    Added media file settings to settings.py and urls.py.

---
+++ urls.py
@@ -0,0 +1,32 @@
+"""
+URL configuration for config project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from config import settings
+from petapp import views
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='home'),
+    path('catalog/', views.catalog, name='catalog'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('auth/', views.auth, name='auth'),
+    path('reg/', views.reg, name='reg'),
+    path('basket/', views.basket, name='basket'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
