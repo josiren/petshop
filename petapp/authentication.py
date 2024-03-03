17d21ac4b95e2567dd39746a89ddd12991d32f86
Madara <122021584+Dina-Tolkina@users.noreply.github.com>
2024-03-04 02:37:48 +0300

    add

---
+++ petapp/authentication.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+class EmailAuthBackend(ModelBackend):
+    def authenticate(self, request, email=None, password=None, **kwargs):
+        try:
+            user = User.objects.get(email=email)  # Используйте email вместо username
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            return None
