24daca780059d462e24ccb2a327e6ec600209139
fundaveover <kgpk2@outlook.com>
2024-03-27 12:32:46 +0300

    fasdfasdf

--- config/settings.py
+++ config/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'Pet_store',
         'USER': 'postgres',
         'PASSWORD': '111',
-        'HOST': 'localhost',
+        'HOST': '192.168.27.5',
         'PORT': 5432,
     }
 }
